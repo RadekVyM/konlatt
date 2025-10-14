1d060dd3111d5cfd602f7f1b6376d27e9b07e697
Radek Vymětalík <rvymetalik@email.cz>
2025-10-14 21:31:41 +0200

    Layered layout fixes and refacotring

--- src/cpp/layout/layeredLayout.cpp
+++ src/cpp/layout/layeredLayout.cpp
@@ -26,10 +26,12 @@ void addDummiesToLayers(
     int conceptsCount,
     const std::vector<std::unordered_set<int>>& subconceptsMapping,
     const std::vector<int>& layersMapping,
-    std::vector<int>& horizontalCoords,
+    std::vector<int>& horizontalPositions,
     std::vector<std::vector<int>>& layersWithDummies,
     std::unordered_map<int, std::vector<int>>& dummySubconceptsMapping,
-    std::unordered_map<int, std::vector<int>>& dummySuperconceptsMapping
+    std::unordered_map<int, std::vector<int>>& subconceptsToRemove,
+    std::unordered_map<int, std::vector<int>>& dummySuperconceptsMapping,
+    std::unordered_map<int, std::vector<int>>& superconceptsToRemove
 ) {
     int newDummy = conceptsCount;
 
@@ -40,62 +42,94 @@ void addDummiesToLayers(
             int toLayer = layersMapping[to];
             int diff = abs(toLayer - fromLayer);
 
-            if (diff > 1) {
-                // add dummies 
-                int previousSuperconcept = from;
+            if (diff <= 1) {
+                // The layers are neighboring, no dummies need to be added
+                continue;
+            }
 
-                for (int i = 1; i <= diff - 1; i++) {
-                    float ratio = (float)i / (toLayer - fromLayer);
-                    int coord = (int)round((ratio * (horizontalCoords[to] - horizontalCoords[from])) + horizontalCoords[from]);
-                    std::vector<int>& targetLayer = layersWithDummies[fromLayer + i];
+            // Dummies need to be added 
+            int previousSuperconcept = from;
 
-                    // TODO: this part is weird and causes problems
-                    // different behavior of C++ datastructures (probably) causes problems overall
-                    if (coord + 1 > targetLayer.size()) {
-                        targetLayer.push_back(newDummy);
-                    }
-                    else {
-                        targetLayer.insert(targetLayer.begin() + coord, newDummy);
-                    }
-                    horizontalCoords.push_back(coord);
+            for (int i = 1; i <= diff - 1; i++) {
+                float ratio = (float)i / (toLayer - fromLayer);
+                int newDummyHorizontalPosition = (int)round(
+                    (ratio * (horizontalPositions[to] - horizontalPositions[from])) + horizontalPositions[from]);
+                std::vector<int>& targetLayer = layersWithDummies[fromLayer + i];
 
-                    for (int j = coord + 1; j < targetLayer.size(); j++) {
-                        horizontalCoords[targetLayer[j]]++;
-                    }
+                // TODO: this part is weird and causes problems
+                // different behavior of C++ datastructures (probably) causes problems overall
+                if (newDummyHorizontalPosition + 1 > targetLayer.size()) {
+                    targetLayer.push_back(newDummy);
+                }
+                else {
+                    targetLayer.insert(targetLayer.begin() + newDummyHorizontalPosition, newDummy);
+                }
+                horizontalPositions.push_back(newDummyHorizontalPosition);
 
-                    addSubconceptToMapping(dummySubconceptsMapping, previousSuperconcept, newDummy);
-                    addSubconceptToMapping(dummySuperconceptsMapping, newDummy, previousSuperconcept);
-
-                    previousSuperconcept = newDummy;
-                    newDummy++;
+                // Move nodes that follow the newly inserted dummy
+                for (int j = newDummyHorizontalPosition + 1; j < targetLayer.size(); j++) {
+                    horizontalPositions[targetLayer[j]]++;
                 }
 
-                addSubconceptToMapping(dummySubconceptsMapping, previousSuperconcept, to);
-                addSubconceptToMapping(dummySuperconceptsMapping, to, previousSuperconcept);
+                addSubconceptToMapping(dummySubconceptsMapping, previousSuperconcept, newDummy);
+                addSubconceptToMapping(dummySuperconceptsMapping, newDummy, previousSuperconcept);
+
+                previousSuperconcept = newDummy;
+                newDummy++;
             }
+
+            addSubconceptToMapping(dummySubconceptsMapping, previousSuperconcept, to);
+            addSubconceptToMapping(dummySuperconceptsMapping, to, previousSuperconcept);
+
+            addSubconceptToMapping(subconceptsToRemove, from, to);
+            addSubconceptToMapping(superconceptsToRemove, to, from);
+        }
+    }
+}
+
+void mergeMappings(
+    std::vector<std::unordered_set<int>>& mapping,
+    std::unordered_map<int, std::vector<int>>& dummyMapping
+) {
+    for (const auto& [concept, subconcepts] : dummyMapping) {
+        for (auto subconcept : subconcepts) {
+            mapping[concept].insert(subconcept);
+        }
+    }
+}
+
+void removeNodesFromMapping(
+    std::vector<std::unordered_set<int>>& mapping,
+    std::unordered_map<int, std::vector<int>>& nodesToRemove
+) {
+    for (const auto& [node, subnodes] : nodesToRemove) {
+        for (auto subnode : subnodes) {
+            mapping[node].erase(subnode);
         }
     }
 }
 
 std::unique_ptr<std::tuple<
     std::vector<std::vector<int>>,
-    std::vector<int>,
-    std::unordered_map<int, std::vector<int>>,
-    std::unordered_map<int, std::vector<int>>
+    std::vector<int>
 >> addDummies(
     int conceptsCount,
-    const std::vector<std::unordered_set<int>>& subconceptsMapping,
+    std::vector<std::unordered_set<int>>& subconceptsMapping,
+    std::vector<std::unordered_set<int>>& superconceptsMapping,
     std::vector<std::unordered_set<int>>& layers,
     const std::vector<int>& layersMapping
 ) {
     auto result = std::make_unique<std::tuple<
         std::vector<std::vector<int>>,
-        std::vector<int>,
-        std::unordered_map<int, std::vector<int>>,
-        std::unordered_map<int, std::vector<int>>>>();
-    auto& [layersWithDummies, horizontalCoords, dummySubconceptsMapping, dummySuperconceptsMapping] = *result;
+        std::vector<int>>>();
+    auto& [layersWithDummies, horizontalPositions] = *result;
 
-    horizontalCoords.resize(conceptsCount);
+    std::unordered_map<int, std::vector<int>> dummySubconceptsMapping;
+    std::unordered_map<int, std::vector<int>> subconceptsToRemove;
+    std::unordered_map<int, std::vector<int>> dummySuperconceptsMapping;
+    std::unordered_map<int, std::vector<int>> superconceptsToRemove;
+
+    horizontalPositions.resize(conceptsCount);
     layersWithDummies.resize(layers.size());
 
     int maxLayerSize = maxSizeOfSets(layers);
@@ -104,12 +138,15 @@ std::unique_ptr<std::tuple<
         std::unordered_set<int>& layer = layers[i];
         float offset = (float)(maxLayerSize - layer.size()) / 2;
 
+        // Set initial horizontal positions of the nodes in the current layer,
+        // so that the layer is aligned with the center vertical axis
         int j = 0;
         for (auto value : layer) {
-            horizontalCoords[value] = j + offset;
+            horizontalPositions[value] = j + offset;
             j++;
         }
 
+        // Copy the current layer to the new collection that will contain dummies too
         layersWithDummies[i].insert(layersWithDummies[i].end(), layer.begin(), layer.end());
     }
 
@@ -117,10 +154,22 @@ std::unique_ptr<std::tuple<
         conceptsCount,
         subconceptsMapping,
         layersMapping,
-        horizontalCoords,
+        horizontalPositions,
         layersWithDummies,
         dummySubconceptsMapping,
-        dummySuperconceptsMapping);
+        subconceptsToRemove,
+        dummySuperconceptsMapping,
+        superconceptsToRemove);
+
+    // Add dummies to the cover relation mappings
+    subconceptsMapping.resize(horizontalPositions.size());
+    superconceptsMapping.resize(horizontalPositions.size());
+    mergeMappings(subconceptsMapping, dummySubconceptsMapping);
+    mergeMappings(superconceptsMapping, dummySuperconceptsMapping);
+
+    // Remove the transitive relations from the mappings
+    removeNodesFromMapping(subconceptsMapping, subconceptsToRemove);
+    removeNodesFromMapping(superconceptsMapping, superconceptsToRemove);
 
     // Make the coords precise
     int maxWithDummies = maxSizeOfVectors(layersWithDummies);
@@ -130,8 +179,10 @@ std::unique_ptr<std::tuple<
         float offset = (float)(maxWithDummies - layer.size()) / 2;
         int j = 0;
 
+        // Set correct horizontal positions of the nodes in the current layer,
+        // so that the layer is aligned with the center vertical axis
         for (auto value : layer) {
-            horizontalCoords[value] = j + offset;
+            horizontalPositions[value] = j + offset;
             j++;
         }
     }
@@ -142,11 +193,9 @@ std::unique_ptr<std::tuple<
 std::unique_ptr<std::vector<std::vector<int>>> reduceCrossingsUsingAverage(
     int conceptsCount,
     std::vector<std::vector<int>>& layers,
-    std::vector<int>& horizontalCoords,
+    std::vector<int>& horizontalPositions,
     std::vector<std::unordered_set<int>>& firstMapping,
     std::vector<std::unordered_set<int>>& secondMapping,
-    std::unordered_map<int, std::vector<int>>& firstDummyMapping,
-    std::unordered_map<int, std::vector<int>>& secondDummyMapping,
     bool topToBottom,
     bool useBoth
 ) {
@@ -156,7 +205,7 @@ std::unique_ptr<std::vector<std::vector<int>>> reduceCrossingsUsingAverage(
     int second = topToBottom ? 1 : layers.size() - 2;
     int increase = topToBottom ? 1 : -1;
 
-    averages.resize(horizontalCoords.size());
+    averages.resize(horizontalPositions.size());
     reducedLayers->resize(layers.size());
 
     (*reducedLayers)[first].insert((*reducedLayers)[first].begin(), layers[first].begin(), layers[first].end());
@@ -167,53 +216,30 @@ std::unique_ptr<std::vector<std::vector<int>>> reduceCrossingsUsingAverage(
     for (int i = second; i < layers.size() && i >= 0; i += increase) {
         std::vector<int>& layer = layers[i];
 
-        for (int concept : layer) {
+        for (int node : layer) {
             int sum = 0;
             int count = 0;
 
-            if (concept < conceptsCount) {
-                // TODO: redundant code
-                for (int subconcept : firstMapping[concept]) {
-                    sum += horizontalCoords[subconcept];
-                }
-
-                count += firstMapping[concept].size();
-
-                if (useBoth) {
-                    for (int subconcept : secondMapping[concept]) {
-                        sum += horizontalCoords[subconcept];
-                    }
-
-                    count += secondMapping[concept].size();
-                }
-            }
-
             // TODO: redundant code
-            if (firstDummyMapping.count(concept) > 0) {
-                auto& subconcepts = firstDummyMapping[concept];
-
-                for (int subconcept : subconcepts) {
-                    sum += horizontalCoords[subconcept];
-                }
-
-                count += subconcepts.size();
+            for (int subconcept : firstMapping[node]) {
+                sum += horizontalPositions[subconcept];
             }
 
-            if (useBoth && secondDummyMapping.count(concept) > 0) {
-                auto& subconcepts = secondDummyMapping[concept];
+            count += firstMapping[node].size();
 
-                for (int subconcept : subconcepts) {
-                    sum += horizontalCoords[subconcept];
+            if (useBoth) {
+                for (int subconcept : secondMapping[node]) {
+                    sum += horizontalPositions[subconcept];
                 }
 
-                count += subconcepts.size();
+                count += secondMapping[node].size();
             }
 
-            averages[concept] = (float)sum / count;
+            averages[node] = (float)sum / count;
         }
 
         auto& reducedLayer = (*reducedLayers)[i] = std::vector<int>(layer.begin(), layer.end());
-        int offset = horizontalCoords[layer[0]];
+        int offset = horizontalPositions[layer[0]];
 
         // Future self, be aware of the strict weak ordering! You are welcome!
         std::sort(reducedLayer.begin(), reducedLayer.end(), [&](int a, int b) {
@@ -221,7 +247,7 @@ std::unique_ptr<std::vector<std::vector<int>>> reduceCrossingsUsingAverage(
         });
 
         for (int j = 0; j < reducedLayer.size(); j++) {
-            horizontalCoords[reducedLayer[j]] = j + offset;
+            horizontalPositions[reducedLayer[j]] = j + offset;
         }
     }
 
@@ -268,9 +294,10 @@ void computeLayeredLayout(
     auto dummiesResult = addDummies(
         conceptsCount,
         subconceptsMapping,
+        superconceptsMapping,
         layers,
         layersMapping);
-    auto& [layersWithDummies, horizontalCoords, dummySuperconceptsMapping, dummySubconceptsMapping] = *dummiesResult;
+    auto& [layersWithDummies, horizontalCoords] = *dummiesResult;
 
     auto orderedLayers = reduceCrossingsUsingAverage(
         conceptsCount,
@@ -278,8 +305,6 @@ void computeLayeredLayout(
         horizontalCoords,
         superconceptsMapping,
         subconceptsMapping,
-        dummySuperconceptsMapping,
-        dummySubconceptsMapping,
         true,
         false);
     orderedLayers = reduceCrossingsUsingAverage(
@@ -288,8 +313,6 @@ void computeLayeredLayout(
         horizontalCoords,
         subconceptsMapping,
         superconceptsMapping,
-        dummySubconceptsMapping,
-        dummySuperconceptsMapping,
         false,
         false);
     orderedLayers = reduceCrossingsUsingAverage(
@@ -298,8 +321,6 @@ void computeLayeredLayout(
         horizontalCoords,
         superconceptsMapping,
         subconceptsMapping,
-        dummySuperconceptsMapping,
-        dummySubconceptsMapping,
         true,
         true);
 
